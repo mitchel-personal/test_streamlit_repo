87cb041870646b82c3aaee45bcceac45455baeb3
Mitchel Carreon <117309729+mitchel-personal@users.noreply.github.com>
2025-03-07 19:51:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('this is a title editted')
 st.header('This is a header')
 st.markdown('deploy this thing via github')
+
+df = pd.read_csv('./test_file.csv')
+st.area_chart(df)
